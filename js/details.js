77c73301538cb6ac547462cfbceaa33b32e38819
Santi <jsanti.restrepo.2021@gmail.com>
2023-03-08 08:07:37 -0500

    filtros hechos, faltan filtro cruzado

---
+++ js/details.js
@@ -0,0 +1,44 @@
+// Funcion de selectores
+const $ = selector => document.querySelector(selector);
+
+//cambio en la URL
+const params = new URLSearchParams(location.search);
+const id = params.get("id");
+const contieneId = data.events.find(elemento => elemento._id === id) 
+
+// varibales selecionadas
+const $details = $("#detalles");
+
+//inputs check template
+const crearDetail = dato => `<div class="border border-primary rounded mt-5 mb-5 ms-5 me-5">
+    <div class="row">
+        <div class="col-12 col-md-6 d-flex align-items-center" id="imagen-detalles">
+            <img src="${dato.image}" alt="${dato.name}">
+        </div>
+        <div class="col-12 col-md-6 text-center d-flex flex-column justify-content-between">
+            <div>
+                <h3 class="pt-2">${dato.name}</h3>
+                <p class="pt-4 pe-2">${dato.description}</p>
+                <p>Category: ${dato.category}</p>
+                <p>Place: ${dato.place}</p>
+                <p>Capacity: ${dato.capacity}</p>
+                <p>Assistance: ${dato.assistance}</p>
+                <p>price: $${dato.price}</p>
+            </div>
+            <div>
+                <a href="../index.html" class="btn btn-success mb-1 me-2 ms-2 mt-2 d-block">Home</a>
+                <a href="../pages/upcoming.html" class="btn btn-info mb-1 me-2 ms-2 mt-2 d-block">Upcoming Events</a>
+            </div>
+        </div>
+    </div>
+</div>
+    `
+
+// Tarjeta innerHTML
+function ponerTarjetas( obj, element ){
+    let template = '';
+    template += crearDetail( obj )
+    element.innerHTML = template;
+}
+
+ponerTarjetas(contieneId,$details)
---
+++ js/past.js
@@ -0,0 +1,81 @@
+// Funcion de selectores
+const $ = selector => document.querySelector(selector);
+
+// varibales sellecionadas
+const $cards_past = $( "#cards-past" );
+const $checkbox_Selector = $("#checkbox-selector");
+
+// Datos según categoria
+const listaCategorias = Array.from( new Set( data.events.map( event => event.category ) ) )
+
+//inputs check template
+const crearChecked = dato => `<div class="form-check form-check-inline check-box">
+        <input class="form-check-input" type="checkbox" id="${dato}" value="${dato}">
+        <label class="form-check-label" for="${dato}">${dato}</label>
+    </div>
+    `
+
+// tarjetas template
+const crearTarjeta = array =>
+        `<div class="col-12 col-md-6 col-lg-3 mb-3">
+        <div class="card shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+            <img src="${array.image}" class="card-img-top" alt="${array.name}">
+            <div class="card-body d-flex flex-column justify-content-between">
+                <div>
+                    <div class="d-flex justify-content-around">
+                        <span class="badge text-bg-primary ">${array.place}</span>
+                    </div>
+                    <h5 class="card-title text-center mt-2">${array.name}</h5>
+                    <p class="card-text text-center">${array.description}</p>
+                </div>
+                <div class="d-flex justify-content-between align-items-center">
+                    <p class="card-text ">Price $${array.price}</p>
+                    <a href="./details.html?id=${array._id}" class="btn btn-info">Info</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+
+//Checked innerHTML
+function ponerChecked( array, element ){
+    let template = '';
+    for( let event of array ){
+        template += crearChecked( event )
+    }
+    element.innerHTML = template;
+}
+
+function filtradoPast(array){
+    return array.filter(event => event.date < data.currentDate);
+}
+
+// Tarjetas innerHTML
+function ponerTarjetas(array, element){
+    let template = '';
+    for( let event of array ){
+            template += crearTarjeta( event )
+        }
+    element.innerHTML = template;
+}
+
+// Eventos
+$checkbox_Selector.addEventListener( 'change', e => ponerTarjetas(filtroCheckBox(filtradoPast(data.events)),$cards_past))
+
+//filtrados
+function filtroCheckBox( array ){
+    const $check_box = Array.from(document.querySelectorAll('input[type=checkbox]:checked'));
+    const arrayValues = $check_box.map(e => e.value);
+    if (arrayValues.length === 0){
+        return array;
+    }
+    const arrayFiltrado = array.filter(event => {
+        return arrayValues.includes(event.category)
+        }
+    )
+    console.log(arrayFiltrado)
+    return arrayFiltrado;
+} 
+    
+ponerTarjetas( filtradoPast(data.events) , $cards_past );
+ponerChecked(listaCategorias , $checkbox_Selector);
